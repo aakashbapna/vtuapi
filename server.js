6b104eb2cf193ae9524ea0a8bc23a1d3382486bd
Aakash <aakash@aakash-desktop.(none)>
2011-02-11 17:24:58 +0530

    added

--- server.js
+++ server.js
@@ -280,7 +280,7 @@ app.tcp_server_send = function (location,data) {
 }
 tcp_server.listen(8123);
 
-var http_server = require('http');
+var http = require('http');
  
 var http_server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })

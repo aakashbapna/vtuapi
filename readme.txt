2e15cdab5dd56aae20c02cbdfa226f2b10d36106
Aakash <bapna@acm.org>
2011-04-18 19:36:57 +0530

    lots of changes

--- readme.txt
+++ readme.txt
@@ -7,13 +7,25 @@ Very straight forward -
 	
 
 Multiple fetch in single api call -
-	* Give up to 100 USNs seperated by comma to fetch in parallel!
+	* Give up to 50 USNs seperated by comma to fetch in parallel!
 		eg- http://vtuapi.aakash.org/results.json?usn=1pe07is000,1pe07cs000,1pe07ec000
 		
 	* For you all lazy people- automatically increment USN and fetch results, use as:
-		eg- http://vtuapi.aakash.org/results.json?usn=1pe07is000:100
-			fetches results of ALL USN from 1pe07is000 to 1pe07is100 !!
+		eg- http://vtuapi.aakash.org/results.json?usn=1pe07is000:50
+			fetches results of ALL USN from 1pe07is000 to 1pe07is050 !!
 
+Bypass cache and fetch fresh result from VTU server -
+	* Add a reload=true parameter to the query string.
+		eg- http://vtuapi.aakash.org/results.json?usn=1pe07is000&reload=true
+		
+Rate Limiting -
+	* You can make at the most 200 calls per hour to the API.
+	* Beyond that it will ask you to wait before making more requests.
+	* You can get api calls remaining and seconds to reset information in HTTP response headers.
+
+Source code -
+	-- will update soon with a link to github repo --
+		
 Notes -
 	* Shows complete vtu result including backlogs.
 	* Very fast, developed with nodejs, uses redis for server side caching of results.
@@ -27,7 +39,7 @@ List of cool mashups using vtu api:
 
 
 DISCLAIMLER - 
-	* I am not responsible for wrong results reported.
-	* Don't abuse the api.
+	* This service uses data that is already public, we just apply transformations to it and make it available in a better way. 
+	* We are not responsible for wrong results reported.
 	* The service may break anytime. (yes.sadly.)
 	
--- server.js
+++ server.js
@@ -5,30 +5,64 @@ var app = require('express').createServer(),
  os = require("os"),
  Do = require("./do"),
  redis_pkg = require("redis");
-/*jsdom.defaultDocumentFeatures = {
-   FetchExternalResources   : false, 
-   ProcessExternalResources : false,
-   MutationEvents           : false,
-   QuerySelector            : false
- }*/
 
+var MAX_USNS = 50;
+var MAX_REQUEST_HOUR = 200;
 var redis = redis_pkg.createClient();
 
 app.get('/', function(req, res){
     res.sendfile('readme.txt');
 });
 
+//check for rate limiting in this handler
+app.get("/results.json", function(req,res,next){
+	redis.exists("global:rate_limit:hour:"+req.connection.remoteAddress,function(err,reply){
+		if (reply == 0 ) {
+			//first request from this ip in this hour.
+			redis.setex("global:rate_limit:hour:"+req.connection.remoteAddress,3600,MAX_REQUEST_HOUR - 1) // hourly rate limit hence expire = 86400 secs
+			res.header("X-RateLimit-remaining",MAX_REQUEST_HOUR - 1)
+			res.header("X-RateLimit-reset",3600);
+			next();
+		} else  {
+		  	redis.decr("global:rate_limit:hour:"+req.connection.remoteAddress, function(err, request_remaining) {
+				if (request_remaining<0) {
+				    res.header("X-RateLimit-remaining", 0)
+				    setRateLimitTTL(function(ttl){
+				    	res.send("API rate limiting in effect, please make request after "+Math.ceil(ttl/60)+" mins",400)
+				    })
+				 } else {
+				    res.header("X-RateLimit-remaining",request_remaining)
+				    setRateLimitTTL(function(ttl){
+				    	next();
+				    })
+				 	
+				 }
+			   }) ; 
+		
+		function setRateLimitTTL(callback){
+			redis.ttl("global:rate_limit:hour:"+req.connection.remoteAddress, function(err,ttl){
+						 	res.header("X-RateLimit-reset",ttl)
+							callback(ttl);	 		
+						 })
+		
+			}
+		}
+	})
+
+})
+
+//main api request handler
 app.get('/results.json', function(req, res){
 	
 	if(!req.query.usn){
 		res.send("usn parameter missing",400);
+		return;
 	}
 	var usn_arr = []
 	if(req.query.usn.indexOf(":")>0) {
 	    var starting_usn = req.query.usn.split(":",2)[0]
 		var total_required = parseInt(req.query.usn.split(":",2)[1])
-		total_required = total_required>100 ?  100 : total_required
-		 
+		total_required = total_required> MAX_USNS ?  MAX_USNS : total_required
 		for(var i=0;i<total_required;i++) 
 		{
 			usn_arr.push(starting_usn)
@@ -36,43 +70,33 @@ app.get('/results.json', function(req, res){
 		}		
 	} else
 		usn_arr = req.query.usn.split(",")
-	console.log("request for usn:"+ usn_arr);
+	redis.incr("global:total_requests",function(err,total_now){
+		console.log("request no: "+ total_now +" for usn:"+ usn_arr);
+	})
+	
     res.contentType('json');
+    
+    //force reload of result if reload=true in querystring
+    var reload_cache = false
+    if(req.query.reload && req.query.reload == "true")
+    	reload_cache = true;
+    
+    
     if(usn_arr.length == 1) {
-		vtu_result(usn_arr[0],function(json){
-		if(req.query.__debug)
-		{
-			json.loadavg = os.loadavg()
-			json.freemem = os.freemem()/1024
-		}
-			res.send(json);
+		vtu_result(usn_arr[0],reload_cache, function(json) {
+		  res.send(json);
 		})
     } else {
-    usn_arr = usn_arr.slice(0,100);
-    var actions = usn_arr.map(function (usn) {
-					  return vtu_result_multi(usn);
-					});
-	Do.parallel(actions)(function(results){
-			//console.log(results);
-			res.send(results);
-		})
+		usn_arr = usn_arr.slice(0,MAX_USNS);
+		var actions = usn_arr.map(function (usn) {
+						  return vtu_result_multi(usn,reload_cache);
+						});
+		Do.parallel(actions) (function(results){
+				//console.log(results);
+				res.send(results);
+			})
     
-    }
-   
-
-   
-    
-	/*scraper(request_params, function(err, $) {
-			 	if (err) {
-			 		sys.log(err)	
-			 	} else {
-					 var result_html = $('html').html()
-					 res.send(result_html);
-			 	}
-			}
-	);*/
-
-    
+    }    
 });
 
 app.listen(process.argv[2]||80)
@@ -85,10 +109,10 @@ function vtu_result_multi(usn) {
 
 }
 
-function vtu_result(usn,callback) {
+function vtu_result(usn, reload_cache, callback) {
 	redis.get("results:"+usn.toUpperCase(),function(err,json){
-		if(err || !json) {
-		 //not there fetch new
+		if(err || !json || reload_cache) {
+		 //not there or force reload, fetch new
 			request_params = {
 			   'uri': 'http://results.vtu.ac.in/vitavi.php',
 			   'headers': {
@@ -103,13 +127,12 @@ function vtu_result(usn,callback) {
 			request(request_params
 			 , function (error, response, body) {
 			  if(response.statusCode == 200) {
-				body = body.replace(/[<]script .*[>][\s\S]*[<][/]script[>]/gi,"")
-				//res.send(body)
+				body = body.replace(/[<]script .*[>][\s\S]*[<][/]script[>]/gi,"") // VTU page has js error!, remove all script tags to init jsdom properly
 			  	var window = jsdom.jsdom(body).createWindow();
-			  	console.log("created dom window");
+			  	//console.log("created dom window");
 			  	jsdom.jQueryify(window, __dirname+'/jquery.js', function (window, $) {
-			  	  console.log("jqueryfied!")
-				  var $t = $('table[bgcolor="#ffffff"] tr td[width="513"]').eq(0)
+			  	  //console.log("jqueryfied!")
+				  var $t = $('table[bgcolor="#ffffff"] tr td[width="513"]').eq(0) // thats the result table!
 				  json.name = $t.find("b").eq(0).text().replace(/[(].*[)]/,"").trim().toProperCase()
 				  if(json.name==""||!json.name) {
 				  	  delete json.name
@@ -123,12 +146,11 @@ function vtu_result(usn,callback) {
 				  {
 				  	var result_obj = {}
 				  	result_obj.semester = parseInt(result_tables.eq(i).find("b").eq(1).text())
-				  	//console.log(i);
 				  	result_obj.status = result_tables.eq(i).find("b").eq(2).text().split(":",2)[1].toLowerCase().trim()
 				  	result_obj.subjects = []
 				  	result_tables.eq(i+1).find("tr").each(function(index){
 				  		if(index==0)
-				  			return;
+				  			return; //first row is coloumn names!
 				  		var subject_obj = {}
 				  		var $cells = $(this).find("td");
 				  		var subject_text = $cells.eq(0).text();
@@ -146,21 +168,20 @@ function vtu_result(usn,callback) {
 				  }
 				  //console.log(json)
 				  callback(json);
-				  redis.setex("results:"+json.usn,864000,JSON.stringify(json))
+				  redis.setex("results:"+json.usn, 3*30*86400, JSON.stringify(json)) // cache results for 3 months
 				});
 				
 			  } else {
 				json.error = 'VTU server status code '+ response.statusCode;
-				//console.log('error: '+ response.statusCode);
+				console.log('VTU error: '+ response.statusCode);
 				//console.log(body);
 				callback(json);
-				
 			  }
 			})
 		 } else {
 		 //its there in  redis cache
 		 	callback(JSON.parse(json))
-		 	console.log("redis cache hit for "+usn);
+		 	//console.log("redis cache hit for "+usn);
 		 }
 		 
 	
@@ -170,6 +191,8 @@ function vtu_result(usn,callback) {
 	
 
 }
+
+
 
 function increment_usn(usn) {
 	var match = /([\d]{3})/.exec(usn)
@@ -184,9 +207,8 @@ String.prototype.toProperCase = function() {
       function($1) { return $1.toUpperCase(); });
 };
 
-String.prototype.trim = function(){
+String.prototype.trim = function() {
 	return this.replace(/^\s+|\s+$/g, "");
 };
 
-var utils = {}
 
